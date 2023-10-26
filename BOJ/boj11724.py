f9fd43ccc11fb57a46f79832dd0c3f6142e98d1c
JH <niinp28@gmail.com>
2023-10-27 00:26:42 +0900

    1027 code fix

--- BOJ/boj11724.py
+++ BOJ/boj11724.py
@@ -1,13 +1,10 @@
 # 연결 요소의 갯수
 def dfs(n):
-    global cnt
     visited[n] = 1
     for node in graph[n]:
         if not visited[node]:
             dfs(node)
         
-    
-    
 
 n, m = map(int, input().split())
 graph = {}
@@ -18,10 +15,16 @@ for _ in range(m):
     graph[u-1] = graph.get(u-1, []) + [v-1]
     graph[v-1] = graph.get(v-1, []) + [u-1]
 
-for i in range(n):
-    if m == 0: break
-    if not visited[i]:
-        dfs(i)
-        cnt += 1
+if m == 0:
+    print(n)
+else:
+    for i in range(n):
+        if not visited[i]:
+            if graph.get(i) == None:
+                visited[i] = 1
+                cnt += 1
+            else:
+                dfs(i)
+                cnt += 1
 
-print(cnt)
+    print(cnt)

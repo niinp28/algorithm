05eafd9e051f5e981050ead593fa46294dd79b12
JH <niinp28@gmail.com>
2023-12-01 23:56:42 +0900

    1201_1325번 아직도 못품

--- BOJ/BOJ1325.py
+++ BOJ/BOJ1325.py
@@ -1,37 +1,40 @@
 # 효율적인 해킹
 from collections import deque
-def bfs(start, visited):
-    global cnt
+def bfs(graph, start):
+    visited = [0] * (N+1)
     q = deque()
     q.append(start)
-    visited.append(start)
+    visited[start] = 1
+    cnt = 1
     while q:
         s = q.popleft()
         if graph.get(s) == None:
             break
         else:
-            for computer in graph[s]:
-                if computer not in visited:
-                    visited.append(computer)
-                    q.append(computer)
+            for i in graph[s]:
+                if not visited[i]:
+                    q.append(i)
+                    visited[i] = 1
                     cnt += 1
+    return cnt
 
 
 N, M = map(int, input().split()) # N <= 10,000, M <= 100,000
 graph = {}
-answer = []
+
 for _ in range(M):
     a, b = map(int, input().split())
     graph[b] = graph.get(b, []) + [a]
 
-for i in graph.keys():
-    cnt = 0
-    bfs(i, [])
-    answer.append((i, cnt))
-answer.sort(key=lambda x: (-x[1], x[0]))
-mx = answer[0][1]
-result = []
-for com, tmp in answer:
-    if tmp == mx:
-        result.append(com)
-print(' '.join(map(str,result)))
+mx = 0
+answer = []
+
+for i in range(1, N+1):
+    cnt = bfs(graph, i)
+    if cnt > mx:
+        mx = cnt
+        answer = [i]
+    elif cnt == mx:
+        answer.append(i)
+
+print(' '.join(map(str,answer)))

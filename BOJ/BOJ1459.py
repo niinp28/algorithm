f6425b1beceb5e2ebca506206c676d56fe8ec160
JH <niinp28@gmail.com>
2023-12-05 23:58:53 +0900

    1205

---
+++ BOJ/BOJ1459.py
@@ -0,0 +1,12 @@
+x, y, w, s = map(int, input().split())
+cnt = 0
+
+a1 = (x+y) * w
+if (x+y) % 2 == 0:
+    a2 = max(x, y) * s
+else:
+    a2 = (max(x, y)-1) * s + w
+
+a3 = (min(x, y) * s) + (abs(x-y) * w)
+
+print(min(a1, a2, a3))

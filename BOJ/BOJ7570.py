3c924b8d001c70f858f522ecd608c06da50b25b3
JH <niinp28@gmail.com>
2024-01-08 23:29:51 +0900

    0108

---
+++ BOJ/BOJ7570.py
@@ -0,0 +1,19 @@
+# 줄 세우기
+
+n = int(input())
+children = [0] + list(map(int, input().split()))
+position = [0] * (n+1)
+for i in range(1, len(children)):
+    position[children[i]] = i
+
+cnt = 1
+mx = -1
+
+for i in range(1, len(children)-1):
+    if position[i] < position[i+1]:
+        cnt += 1
+        if cnt > mx:
+            mx = cnt
+    else:
+        cnt = 1
+print(n-mx)

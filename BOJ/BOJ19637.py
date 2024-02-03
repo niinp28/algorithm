ea2bfb9969d25de76bc0208c0400f0ec02bf3d9c
JH <niinp28@gmail.com>
2024-02-04 00:26:35 +0900

    0203_알고리즘 수정

--- BOJ/BOJ19637.py
+++ BOJ/BOJ19637.py
@@ -1,4 +1,5 @@
 # if문좀 대신써줘
+import sys
 
 N, M = map(int, input().split())  # 10의 5승 이하
 
@@ -6,14 +7,13 @@ score = []
 for i in range(N):
     s, n = input().split()
     n = int(n)
-    if len(score) == 0 or n > score[-1][1]:  # 없으면 넣기, 있으면 기준 점수가 기존에 있던 것 보다 클 때 넣기
-        score.append([s, n])
-score.sort(key=lambda x: int(x[1])) #오름차순
+    score.append([s, n])
+score.sort(key=lambda x: x[1])  # 오름차순
 
 check = [int(input()) for _ in range(M)]
 for char in check:
     start = 0
-    end = N
+    end = N-1
     res = 0
     while start <= end:
         mid = (start + end) // 2
@@ -22,7 +22,7 @@ for char in check:
             end = mid -1
         else:
             start = mid + 1
-    print(score[res][0])
+    print(score[start][0])
 
     
     

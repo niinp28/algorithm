4dc2e825262d695901118b6d6d000aa3e41e4408
JH <niinp28@gmail.com>
2024-02-17 00:18:47 +0900

    0215_8979번 수정

--- BOJ/BOJ8979.py
+++ BOJ/BOJ8979.py
@@ -3,22 +3,12 @@
 N, K = map(int, input().split())
 nations = [list(map(int, input().split())) for _ in range(N)]
 
-nations.sort(key=lambda x: (-x[1], -x[2], -x[3]))
+nations.sort(key=lambda x: (x[1], x[2], x[3]), reverse= True)
 
-graph = {}
-info = nations[0][1:]
-cnt = 1
-increase = 0
-for nation in nations:
-    nation_id = nation[0]
-    tmp = nation[1:]
-    if info == tmp:
-        graph[nation_id] = cnt
-        increase += 1
-    else:
-        cnt += increase
-        graph[nation_id] = cnt
-        info = tmp
-        increase = 0
+idx = [nations[i][0] for i in range(N)].index(K)
 
-print(graph[K])
+
+for i in range(N):
+    if nations[i][1:] == nations[idx][1:]:
+        print(i+1)
+        break

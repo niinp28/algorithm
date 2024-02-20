707cab421d1b9375e5ddd102d4826d1aeed90de6
JH <niinp28@gmail.com>
2024-02-20 23:42:09 +0900

    0220

---
+++ BOJ/BOJ1927.py
@@ -0,0 +1,17 @@
+# 최소 힙
+import heapq
+import sys
+N = int(sys.stdin.readline())
+heap = []
+
+for _ in range(N):
+    x = int(sys.stdin.readline())
+    if x == 0:
+        if heap:
+            a = heapq.heappop(heap)
+            print(a)
+        else:
+            print(0)
+        
+    else:
+        heapq.heappush(heap, x)

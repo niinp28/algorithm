949fdde801cfb815932616d77a4d2cdb77eef474
JH <niinp28@gmail.com>
2024-05-27 00:02:16 +0900

    0526_수정

--- BOJ/BOJ2589.py
+++ BOJ/BOJ2589.py
@@ -1,12 +1,14 @@
 # 보물섬
 from collections import deque
+
 dr = [0, 1, 0, -1]
 dc = [1, 0, -1, 0]
-def bfs(r, c, v):
+def bfs(r, c):
     global mx
     q = deque()
     q.append((r, c))
     tmp = 0
+    v = [[0]*M for _ in range(N)]
     v[r][c] = 1
     while q:
         a, b = q.popleft()
@@ -17,6 +19,8 @@ def bfs(r, c, v):
                 q.append((nr, nc))
                 v[nr][nc] = v[a][b] + 1
                 tmp = max(tmp, v[nr][nc])
+    
+    
     return tmp-1
     
 
@@ -24,10 +28,10 @@ def bfs(r, c, v):
 
 N, M = map(int, input().split())
 arr = [list(input()) for _ in range(N)]
-visited = [[0]*M for _ in range(N)]
+
 res = 0
 for i in range(N):
     for j in range(M):
         if arr[i][j] == 'L':
-            res = max(res, bfs(i, j, visited))
+            res = max(res, bfs(i, j))
 print(res)

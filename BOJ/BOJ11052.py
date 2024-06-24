7ed91c421995ac27365cca44be8563d7b5552cfe
JH <niinp28@gmail.com>
2024-06-24 23:59:19 +0900

    0624

---
+++ BOJ/BOJ11052.py
@@ -0,0 +1,10 @@
+# 카드 구매하기
+
+n = int(input())
+pack = [0] + list(map(int, input().split()))
+dp = [0] * (n+1)
+
+for i in range(1, n+1):
+    for j in range(1, i+1):
+        dp[i] = max(dp[i], pack[j]+dp[i-j])
+print(dp)

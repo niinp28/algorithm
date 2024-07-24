4a642c150459df114cfebc017de07e01f683dc37
JH <niinp28@gmail.com>
2024-07-24 23:48:01 +0900

    0724

--- BOJ/BOJ2512.py
+++ BOJ/BOJ2512.py
@@ -1,22 +1,25 @@
 # 예산
 
 n = int(input())
-lst = list(map(int, input().split()))
-budget = int(input())
+money = list(map(int, input().split()))
+limit = int(input())
 
-s, e = 0, max(lst)
-
+s = 0
+e = max(money)
+ans = 0
 while s <= e:
-    m = (s + e) // 2
-    total = 0
-    for city in lst:
-        if city > m:
-            total += m
+    m = (s+e) // 2 
+    tmp = 0
+    for b in money:
+        if b <= m:
+            tmp += b
         else:
-            total += city
+            tmp += m
     
-    if total <= budget:
-        s = m+1
+    if tmp <= limit:
+        s = m + 1
+        ans = m
     else:
-        e = m-1
-print(e)
+        e = m - 1
+
+print(ans)

c64fab137f920f503a0b0d99ce26593b06ca9fa3
JH <niinp28@gmail.com>
2024-07-28 22:01:11 +0900

    0728

--- BOJ/BOJ2304.py
+++ BOJ/BOJ2304.py
@@ -1,42 +1,44 @@
 # 창고 다각형
-
 n = int(input())
+storage = [tuple(map(int, input().split())) for _ in range(n)]
+storage.sort()
 
-pillar = []
+lst = [0] * 1001
 ans = 0
-for i in range(n) :
-    a,b = map(int,input().split())
-    pillar.append([a,b])
-#기둥을 x축 순으로 정렬한다. 
-pillar.sort()
+start = storage[0][0]
+end = storage[-1][0]
 
-#가장 높은 기둥의 번호를 알아낸다. 
-i = 0
-for l in pillar :
-    if l[1] >ans :
-        ans = l[1]
-        idx = i
-    i += 1
+mxh = 0
+idx = 0
+for L in storage:
+    lst[L[0]] = L[1]
+    if L[1] > mxh:
+        mxh = L[1]
+        idx = L[0]
 
-
-h = pillar[0][1]
-
-
-for i in range(idx) :
-    if h < pillar[i+1][1] :
-        ans += h * (pillar[i+1][0]-pillar[i][0])
-        h = pillar[i+1][1]
-    #아니면 그냥 현재면적을 더해준다. 
-    else :
-        ans += h * (pillar[i+1][0] - pillar[i][0])
-
-h = pillar[-1][1]
-
-for i in range(n-1, idx, -1) :
-    if h < pillar[i-1][1] :
-        ans += h * (pillar[i][0]-pillar[i-1][0])
-        h = pillar[i-1][1]
-    else :
-        ans += h * (pillar[i][0] - pillar[i-1][0])
-
+cur = 0
+for i in range(start, end+1):
+    if i == idx:
+        cur = max(cur, lst[i])
+        ans += cur
+        break
+    cur = max(cur, lst[i])
+    ans += cur
+        
+cur = 0
+for i in range(end, start-1, -1):
+    if i == idx:
+        break
+    cur = max(cur, lst[i])
+    ans += cur
 print(ans)
+'''
+7
+2 4
+11 4
+15 8
+4 6
+5 3
+8 10
+13 6
+'''
